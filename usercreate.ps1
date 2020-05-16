86108f773e7714185cdff4bedb6f7df3ae82a491
henry <henryrohlfing2@gmail.com>
2020-05-16 17:12:29 -0500

    Added Resource on AD Module

--- usercreate.ps1
+++ usercreate.ps1
@@ -1,8 +1,12 @@
-#Welcome to the Tool
+# Script requires the installation of the Active Directory Powershell Module - this article can help you install it if you don't already. 
+# https://docs.microsoft.com/en-us/archive/blogs/ashleymcglone/install-the-active-directory-powershell-module-on-windows-10
+
+
+# Welcome to the Tool
 Write-Host "Welcome to the User Onboarding Tool" -ForegroundColor Yellow
 Write-Host "..................................." -ForegroundColor Yellow
 
-#Endless Loop
+# Endless Loop
 while($i -lt 9999)
 {
     $i++
@@ -14,7 +18,7 @@ while($i -lt 9999)
     Write-Host "${name} has been created...." -ForegroundColor Yellow
     Get-ADUser $name
     
-    #Another User?
+    # Another User?
     $again = Read-Host -Prompt "Would You Like to Create Another User? (y/n)"
     
     if ($again -eq  "y")
